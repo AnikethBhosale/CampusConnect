79bd6d9b16d0ba4a16ef582dc8fdf05c5db77713
Aniketh <anikethbhosale11@gmail.com>
2024-05-04 23:31:57 +0530

    css changes

--- cc_frontend/src/Pages/Home/Home.jsx
+++ cc_frontend/src/Pages/Home/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Home.css'
-import HomeSidebar1 from '../../Components/HomeSidebar/HomeSidebar1'
-import HomeSidebar2 from '../../Components/HomeSidebar/HomeSidebar2'
+//import HomeSidebar1 from '../../Components/HomeSidebar/HomeSidebar1'
+//import HomeSidebar2 from '../../Components/HomeSidebar/HomeSidebar2'
 import HomeFeed from '../../Components/HomeFeed/HomeFeed'
 
 function Home() {
   return (  
+    // <HomeSidebar1/>
+    //<HomeSidebar2/>
      <div className='home-container'>
-       <HomeSidebar1/>
-       <HomeSidebar2/>
+
        <HomeFeed/>
      </div>
   )
